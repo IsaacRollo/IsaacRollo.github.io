168d50a6dde96c84bfe3d694a7a292fd12f17f4b
IsaacRollo <IsaacRollo@gmail.com>
2026-03-02 11:11:36 -0600

    Basic geoJSON implementation

--- js/map.js
+++ js/map.js
@@ -4,7 +4,31 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; OpenStreetMap contributors'
 }).addTo(map);
 
-L.marker([40.7128, -74.0060])
-    .addTo(map)
-    .bindPopup('New York City')
-    .openPopup();
+fetch('data/locations.geojson')
+    .then(res => res.json())
+    .then(data => {
+    L.geoJSON(data, {
+        onEachFeature: function (feature, layer) {
+            const props = feature.properties;
+
+            const media = props.media[0];
+
+            let mediaHTML = '';
+
+            if (media.type === 'image') {
+            mediaHTML = `<img src="${media.src}" width="250">`;
+            }
+
+            const popupContent = `
+                <h3>${props.title}</h3>
+                <p>${props.description}</p>
+                ${mediaHTML}  
+            `;
+
+            layer.bindPopup(popupContent);
+        }
+    }).addTo(map);
+});
+
+
+
